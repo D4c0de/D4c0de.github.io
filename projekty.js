a0d747663d86bce98d525f25b1bdf7ce62cf9974
marcin kuczok <mkuczok6@gmail.com>
2024-12-09 19:18:02 +0100

    add polsl project

--- projekty.js
+++ projekty.js
@@ -32,6 +32,16 @@ class projekt{
         return code;
     };
 };
+projectTab.push(new projekt("Spotify", "spotify.jpg", "C++, SQL, FTP ", `Odtwarzacz muzyki to aplikacja napisana w języku C++ z wykorzystaniem protokołu FTP i bazy danych SQL.
+Umożliwia zarządzanie lokalną biblioteką muzyczną oraz pobieranie utworów z serwera FTP, a także przechowywanie informacji o utworach i playlistach w bazie danych SQL.
+Dzięki temu aplikacja oferuje prostotę użytkowania przy jednoczesnym wsparciu dla dynamicznego zarządzania zawartością.
+Projekt ten połączył moją pasję do muzyki z rozwijaniem umiejętności w pracy z protokołami sieciowymi i bazami danych, tworząc funkcjonalne rozwiązanie,`));
+
+projectTab.push(new projekt("Koder huffmana", "huffman.jpg", "C++, Doxygen", `Koder Huffmana to aplikacja napisana w języku C++, która umożliwia efektywne kompresowanie i dekompresowanie danych
+przy użyciu algorytmu Huffmana. Przy tworzeniu aplikacji po raz pierwszy użyłem narzędzia Doxygen do generowania dokumentacji,
+co pozwoliło mi usystematyzować kod i ułatwić jego zrozumienie. Realizacja tego projektu była świetną okazją do nauki zaawansowanych
+struktur danych i rozwijania umiejętności w tworzeniu profesjonalnej dokumentacji technicznej.,`));
+
 projectTab.push(new projekt("Konkurs Prevac","Prevac.jpg","C++, C#, WindowsForm, Modbus, SQLite ", `Projekt konkursowy dla firmy Prevac to przedsięwzięcie, 
 w którym zaprojektowałem i zaprogramowałem system obsługujący linię produkcyjną. 
 System działa zarówno po stronie komputera, jak i serwera,
